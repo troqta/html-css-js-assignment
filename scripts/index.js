7af37fe5078778361bd937a6ff724d8e6948c73d
Alexander Simeonov <troqta@gmail.com>
2018-06-27 17:07:40 +0300

    fixed button and added text

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,35 @@
 $(document).ready(function () {
-  var totalH = $('#stickyNav').offset().top;
-  $(window).bind('scroll', function () {
-      var vPos = $(window).scrollTop();
+    var totalH = $('#stickyNav').offset().top;
+    $(window).bind('scroll', function () {
+        var vPos = $(window).scrollTop();
 
-      if (totalH < vPos) {
-          $('#stickyNav').css({
-              'position': 'fixed',
-                  'top': 0
-          })
-      } else {
-          $('#stickyNav').css({
-              'position': 'absolute',
-              'top': ''
-          })
-      }
-  });
+        if (totalH < vPos) {
+            $('#stickyNav').css({
+                'position': 'fixed',
+                'top': 0
+            })
+        } else {
+            $('#stickyNav').css({
+                'position': 'absolute',
+                'top': ''
+            })
+        }
+    });
+    $('.recipe-btn').on('click', function () {
+        var $this = $(this);
+        
+        if ($this.html() === 'Show') {
+            $this.html('Hide');
+            $(this).parent().children('.recipe-text').css({
+                'display': 'inline-block'
+            });;
+        } else {
+            $this.html('Show');
+            
+            $(this).parent().children('.recipe-text').css({
+                'display': ''
+            })
+        }
+    });
 });
+

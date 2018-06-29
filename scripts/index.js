08f0818bfb617d2c188cb134cb2885cb12830e5e
Alexander <troqta@gmail.com>
2018-06-29 18:13:21 +0300

    added author page

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     var totalH = $('#stickyNav').offset().top;
     $(window).bind('scroll', function () {
         var vPos = $(window).scrollTop();
@@ -17,29 +18,57 @@ $(document).ready(function () {
             })
         }
     });
+    var showRecipe = function($recipe){
+        $recipe.children('.recipe-text').fadeIn();
+        $recipe.animate({width:showRecipeWidth}, 300);
+    }
+    var hideRecipe = function($recipe){
+        $recipe.children('.recipe-text').fadeOut('fast', function(){
+            $recipe.animate({width: recipeWidth}, 300)
+        });
+    }
     var recipeWidth = '20%';
+    var showRecipeWidth = '60%';
     $('.recipe-btn').on('click', function () {
         var $this = $(this);
         var $recipe = $(this).parent().parent();
-        console.log($recipe.css('width'));
         if ($this.html() === 'Show') {
             $this.html('Hide');
-            
-            $recipe.children('.recipe-text').fadeIn();
-            // $recipe.css({
-            //     'width':'60%'
-            // });
-            
-            $recipe.animate({width:'60%'}, 300);
+            showRecipe($recipe);
+            // $recipe.children('.recipe-text').fadeIn();
+            // $recipe.animate({width:showRecipeWidth}, 300);
         } else {
             $this.html('Show');
-            
-            $recipe.children('.recipe-text').fadeOut('fast', function(){
-                $recipe.animate({width: recipeWidth}, 300)
-            });
-          
-            
+            hideRecipe($recipe);
+            // $recipe.children('.recipe-text').fadeOut('fast', function(){
+            //     $recipe.animate({width: recipeWidth}, 300)
+            // });  
         }
     });
+    var authorName;
+
+    $('#enter-btn').on('click', function(){
+        if($('#author-box').val()===''){
+            alert('Please enter a name or continue as a guest!');
+        }
+        else{
+        authorName = $('#author-box').val();
+        console.log(authorName);
+        $('#header-welcome').html('Welcome, '+authorName+'!');
+        $('#welcome-page').fadeOut('fast', function(){
+            $('body').css({'overflow' : 'auto'});
+        });
+         }
+    });
+    $('#guest-enter-btn').on('click', function(){
+        authorName = 'Guest';
+        $('#header-welcome').html('Welcome, '+authorName+'!');
+        $('#welcome-page').fadeOut('fast', function(){
+            $('body').css({'overflow' : 'auto'});
+        });   
+    });
+    $(window).on('beforeunload', function() {
+        $(window).scrollTop(0);
+     });
 });
 

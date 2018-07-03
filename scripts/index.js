bb2c147375420bfbee59314ff3b3a41235e78382
Alexander <troqta@gmail.com>
2018-07-03 21:07:45 +0300

    added more recipes and fixed styles

--- scripts/index.js
+++ scripts/index.js
@@ -28,43 +28,54 @@ $(document).ready(function () {
 
     $('#enter-btn').on('click', function(){
         if($('#author-box').val()===''){
-            alert('Please enter a name or continue as a guest!');
+            authorName = 'Guest';
         }
         else{
         authorName = $('#author-box').val();
+        }
         $('#header-welcome').html('Welcome, '+authorName+'!');
-        $('#welcome-page').fadeOut('fast', function(){
+        $('#welcome-page').slideUp('fast', function(){
             $('body').css({'overflow' : 'auto'});
             if(authorName!= 'Guest'){
-                $('#create-recipe-form').fadeIn('fast');
+                $('#create-recipe-form').fadeIn('fast', function(){
+                $('#guest-message').addClass('hidden');
+                });
+                
+            }
+            else{
+                $('#guest-message').removeClass('hidden');
             }
         });
-         }
+         
     });
-    $('#guest-enter-btn').on('click', function(){
-        authorName = 'Guest';
-        $('#header-welcome').html('Welcome, '+authorName+'!');
-        $('#welcome-page').fadeOut('fast', function(){
-            $('body').css({'overflow' : 'auto'});
-        });   
-    });
+    // $('#guest-enter-btn').on('click', function(){
+    //     authorName = 'Guest';
+    //     $('#header-welcome').html('Welcome, '+authorName+'!');
+    //     $('#welcome-page').fadeOut('fast', function(){
+    //         $('body').css({'overflow' : 'auto'});
+    //     });   
+    // });
     $(window).on('beforeunload', function() {
         $(window).scrollTop(0);
      });
 
      $('#create-recipe-form').submit(function(){
-        var recipe = $('<article />', {'class': 'recipe'});
+        var recipe = $('<article />', {'class': 'recipe zoom'});
         var name = $('<h2 />').html($('#form-name').val());
-        var imgSrc = $('<img />', {'class': 'zoom'}).attr('src', $('#form-img').val());
-        var text = $('<p />', {'class': 'recipe-text'}).html($('#form-text').val());
+        var imgSrc = $('<img />').attr('src', $('#form-img').val());
+        var textDiv = $('<div />', {'class': 'recipe-text'});
+        var text = $('<p />').html($('#form-text').val());
+        textDiv.append(text);
         var buttonDiv = $('<div />');
         var author = $('<h3 />', {'class':'recipe-author'}).html('Author: '+authorName);
-        var button = $('<a />', {'class':'button recipe-btn', 'href':'#!'}).html('Show');
+        var buttonShow = $('<a />', {'class':'button recipe-btn', 'href':'#!'}).html('Show');
+        var buttonHide = $('<a />', {'class':'button recipe-btn hidden', 'href':'#!'}).html('Hide');
         buttonDiv.append(author);
-        buttonDiv.append(button);
+        buttonDiv.append(buttonShow);
+        buttonDiv.append(buttonHide);
         recipe.append(imgSrc);
         recipe.append(name);
-        recipe.append(text);
+        recipe.append(textDiv);
         recipe.append(buttonDiv);
  
         $(this).trigger('reset');
@@ -78,6 +89,7 @@ $(document).ready(function () {
          $('#header-welcome').html('Welcome!');
          $('#welcome-page').fadeIn('fast');
          $('body').css({'overflow':'hidden'});
+         $('#guest-message').removeClass('hidden');
 
      });
 });
